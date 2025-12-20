3f64d079fd8634810550190d11d3b97f69de356d
ElshadHu <elsadhumbetli079@gmail.com>
2025-12-20 18:32:20 -0500

    Register user router in trpc

--- src/trpc/router.ts
+++ src/trpc/router.ts
@@ -3,12 +3,14 @@ import { repoRouter } from "../server/routers/repoRouter";
 import { healthRouter } from "../server/routers/healthRouter";
 import { dependencyRouter } from "../server/routers/dependencyRouter";
 import { prRouter } from "../server/routers/prRouter";
+import { userRouter } from "../server/routers/userRouter";
 
 export const appRouter = router({
   repo: repoRouter,
   health: healthRouter,
   dependency: dependencyRouter,
   pr: prRouter,
+  user: userRouter,
 });
 
 export type AppRouter = typeof appRouter;
