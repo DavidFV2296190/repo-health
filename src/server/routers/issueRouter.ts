2dd21893170a3d874655afa494ea5187a531e0ed
ElshadHu <elsadhumbetli079@gmail.com>
2025-12-21 20:38:00 -0500

    chore: remove article and question services

--- src/server/routers/issueRouter.ts
+++ src/server/routers/issueRouter.ts
@@ -1,8 +1,6 @@
 import { z } from "zod";
 import { router, publicProcedure } from "../../trpc/init";
 import * as issueService from "../services/issues/analyze";
-import { searchQuestions } from "../services/external/stackOverflowService";
-import { searchByTag } from "../services/external/devToService";
 
 export const issueRouter = router({
   analyze: publicProcedure
@@ -18,32 +16,6 @@ export const issueRouter = router({
         repo: input.repo,
         token: ctx.session?.accessToken,
       });
-      return result;
-    }),
-
-  findResources: publicProcedure
-    .input(
-      z.object({
-        title: z.string(),
-        labels: z.array(z.string()),
-      })
-    )
-    .query(async ({ input }) => {
-      // Extract search terms from title and labels
-      const query = input.title.slice(0, 80);
-      const tags = input.labels.slice(0, 3);
-
-      // Fetch in parallel
-      const [questions, articles] = await Promise.all([
-        searchQuestions(query, tags, 5),
-        searchByTag(tags[0] || "programming", 5),
-      ]);
-
-      const result = {
-        questions,
-        articles,
-      };
-
       return result;
     }),
 });
